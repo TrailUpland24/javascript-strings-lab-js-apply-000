99c166aaa5c8ccb861bb740e6c80dfa0c95b0b35
David Purdy <dave.purdy1@gmail.com>
2020-10-05 15:16:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let\'s talk about space.";
 
+<<<<<<< HEAD
 var conversation = `Let\'s talk about ${topic}.`; 
+=======
+var conversation = `${topic}`; 
+>>>>>>> 1742400b6911d08fffca2eaef300617f6be10a84
